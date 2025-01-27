dfd661fbd14361b392a8ca6be6c1c16d8c07a9bf
yuki <chenjf2020@mail.sustech.edu.cn>
2025-01-27 23:27:21 +0800

    debug: introduce debug func during relocation

--- os/debug.c
+++ os/debug.c
@@ -34,39 +34,47 @@ void print_sysregs()
 
 // Debug function to print pagetable
 // Assume Sv39
-static void vm_print_walk(uint64 va, pagetable_t pgt, int level)
-{
-	if (level == 3)
-		return;
-	for (uint64 i = 0; i < 512; i++) {
-		pte_t *pte = &((pte_t *)pgt)[i];
-		if (*pte & PTE_V) {
-			for (int j = 0; j < level; j++)
-				printf("  ");
-			uint64 iva = va | (i << (12 + 9 * (2 - level)));
-			if ((iva >> 38) & 1) {
-				iva |= (~((1ull << 39) - 1));
-			}
-			printf("[%x], pte[%p]: %p -> %p %c%c%c%c%c%c%c%c\n", i, pte, (void *)iva, PTE2PA(*pte), *pte & PTE_D ? 'D' : '-',
-			       *pte & PTE_A ? 'A' : '-', *pte & PTE_G ? 'G' : '-', *pte & PTE_U ? 'U' : '-', *pte & PTE_X ? 'X' : '-',
-			       *pte & PTE_W ? 'W' : '-', *pte & PTE_R ? 'R' : '-', *pte & PTE_V ? 'V' : '-');
-			if (!((*pte & PTE_R) || (*pte & PTE_W) || (*pte & PTE_X))) {
-				// has next level;
-				uint64 pa = PTE2PA((uint64)*pte);
-				if (!VALID_PHYS_ADDR(pa))
-					panic("invalid next-level pagetable: pa: %p", pa);
-				uint64 kva = PA_TO_KVA(pa);
-				vm_print_walk(iva, (pagetable_t)kva, level + 1);
-			}
-		}
-	}
+static void vm_print_walk(uint64 va, pagetable_t pgt, int level, int nokva) {
+    if (level == 3)
+        return;
+    for (uint64 i = 0; i < 512; i++) {
+        pte_t *pte = &((pte_t *)pgt)[i];
+        if (*pte & PTE_V) {
+            for (int j = 0; j < level; j++) printf("  ");
+            uint64 iva = va | (i << (12 + 9 * (2 - level)));
+            if ((iva >> 38) & 1) {
+                iva |= (~((1ull << 39) - 1));
+            }
+            printf(
+                "[%x], pte[%p]: %p -> %p %c%c%c%c%c%c%c%c\n", i, pte, (void *)iva, PTE2PA(*pte),
+                *pte & PTE_D ? 'D' : '-', *pte & PTE_A ? 'A' : '-', *pte & PTE_G ? 'G' : '-',
+                *pte & PTE_U ? 'U' : '-', *pte & PTE_X ? 'X' : '-', *pte & PTE_W ? 'W' : '-',
+                *pte & PTE_R ? 'R' : '-', *pte & PTE_V ? 'V' : '-');
+            if (!((*pte & PTE_R) || (*pte & PTE_W) || (*pte & PTE_X))) {
+                // has next level;
+                uint64 pa = PTE2PA((uint64)*pte);
+                if (!VALID_PHYS_ADDR(pa))
+                    panic("invalid next-level pagetable: pa: %p", pa);
+                uint64 kva;
+                if (nokva)
+                    kva = pa;
+                else
+                    kva = PA_TO_KVA(pa);
+                vm_print_walk(iva, (pagetable_t)kva, level + 1, nokva);
+            }
+        }
+    }
+}
+void vm_print(pagetable_t __kva pagetable) {
+    printf("=== PageTable at %p ===\n", pagetable);
+    vm_print_walk(0, pagetable, 0, 0);
+    printf("=== END === \n");
 }
 
-void vm_print(pagetable_t __kva pagetable)
-{
-	printf("=== PageTable at %p ===\n", pagetable);
-	vm_print_walk(0, pagetable, 0);
-	printf("=== END === \n");
+void vm_print_tmp(pagetable_t __pa pagetable) {
+    printf("=== Temporary PageTable at %p ===\n", pagetable);
+    vm_print_walk(0, pagetable, 0, 1);
+    printf("=== END === \n");
 }
 
 void mm_print(struct mm *mm)
--- os/debug.h
+++ os/debug.h
@@ -5,4 +5,5 @@
 void print_trapframe(struct trapframe *tf);
 void print_sysregs();
 void vm_print(pagetable_t __kva pagetable);
+void vm_print_tmp(pagetable_t __pa pagetable);
 void mm_print(struct mm* mm);
